0cc4dd9cfda5a8338af1406a213550d81e3b7930
Deemarc Burakitbumrung <Deemarc@thaigertec.local>
2018-08-01 18:16:34 +0700

    change to use svm

--- tree/tree.py
+++ tree/tree.py
@@ -3,13 +3,16 @@
 
 import numpy as np
 import cv2
+import glob
 
+
+from sklearn import svm
 from sklearn import tree
 from sklearn.cross_validation import train_test_split
 
 def ReadData():
     #Data in format [B G R Label] from
-    data = np.genfromtxt('../data/Skin_NonSkin.txt', dtype=np.int32)
+    data = np.genfromtxt('./data/Skin_NonSkin.txt', dtype=np.int32)
 
     labels= data[:,3]
     data= data[:,0:3]
@@ -29,26 +32,25 @@ def TrainTree(data, labels, flUseHSVColorspace):
 
     trainData, testData, trainLabels, testLabels = train_test_split(data, labels, test_size=0.20, random_state=42)
 
-    print trainData.shape
-    print trainLabels.shape
-    print testData.shape
-    print testLabels.shape
+    print(trainData.shape)
+    print(trainLabels.shape)
+    print(testData.shape)
+    print(testLabels.shape)
 
-    clf = tree.DecisionTreeClassifier(criterion='entropy')
+    # clf = tree.DecisionTreeClassifier(criterion='entropy')
+    clf = svm.SVC()
     clf = clf.fit(trainData, trainLabels)
-    print clf.feature_importances_
-    print clf.score(testData, testLabels)
+    # print(clf.feature_importances_)
+    print(clf.score(testData, testLabels))
 
     return clf
 
-def ApplyToImage(path, flUseHSVColorspace):
-    data, labels= ReadData()
-    clf= TrainTree(data, labels, flUseHSVColorspace)
+def ApplyToImage(path,clf, flUseHSVColorspace,counter):
 
     img= cv2.imread(path)
-    print img.shape
+    print(img.shape)
     data= np.reshape(img,(img.shape[0]*img.shape[1],3))
-    print data.shape
+    print(data.shape)
 
     if(flUseHSVColorspace):
         data= BGR2HSV(data)
@@ -57,12 +59,26 @@ def ApplyToImage(path, flUseHSVColorspace):
 
     imgLabels= np.reshape(predictedLabels,(img.shape[0],img.shape[1],1))
 
+    filename = './results/' + str(counter).zfill(3)
     if (flUseHSVColorspace):
-        cv2.imwrite('../results/result_HSV.png',((-(imgLabels-1)+1)*255))# from [1 2] to [0 255]
+        filename = filename + '_result_HSV.png'
+        cv2.imwrite('./results/result_HSV.png',((-(imgLabels-1)+1)*255))# from [1 2] to [0 255]
     else:
-        cv2.imwrite('../results/result_RGB.png',((-(imgLabels-1)+1)*255))
+        filename = filename + '_result_RGB.png'
+    
+    cv2.imwrite(filename,((-(imgLabels-1)+1)*255))
 
 
 #---------------------------------------------
-ApplyToImage("../face.png", True)
-ApplyToImage("../face.png", False)
+
+data, labels= ReadData()
+clf_hsv = TrainTree(data, labels, True)
+clf_rgb = TrainTree(data, labels, False)
+# ApplyToImage("face.png", clf_hsv, True, 0)
+# ApplyToImage("face.png", clf_rgb, False, 0)
+imagesPath = glob.glob('./input/*.jpg')
+counter = 0
+for imagePath in imagesPath:
+    ApplyToImage(imagePath,clf_hsv, True, counter)
+    ApplyToImage(imagePath,clf_rgb, False, counter)
+    counter = counter +1
